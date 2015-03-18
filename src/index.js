b5841778631081fc1a75e7913379fba910c77a74
Carl <carl@carlossus.com>
2015-03-18 20:46:46 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+/// <reference path="typings/tsd.d.ts" />
+var http = require("http");
+var Promise = require("bluebird");
+/**
+ * Sends a 'GET /' request to a server and returns a promise that returns the round trip time in milliseconds.
+ * @param url The destination url. E.g. www.google.com
+ * @param port Optional: The port number of the destination. Defaults to 80
+ * @returns Promise<{responseTime: number }> A promise that returns the round trip time in milliseconds. Returns -1 if an error occurred.
+ * */
+function ping(url, port) {
+    var promise = new Promise(function (resolve) {
+        var result;
+        var options = { host: url, port: port || 80, path: '/' };
+        var start = Date.now();
+        var pingRequest = http.request(options, function () {
+            result = Date.now() - start;
+            resolve(result);
+            pingRequest.abort();
+        });
+        pingRequest.on("error", function () {
+            result = -1;
+            resolve(result);
+            pingRequest.abort();
+        });
+        pingRequest.write("");
+        pingRequest.end();
+    });
+    return promise;
+}
+module.exports = ping;
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+/// <reference path="typings/tsd.d.ts" />
+
+import http = require("http");
+import Promise = require("bluebird");
+export = ping;
+
+/**
+ * Sends a 'GET /' request to a server and returns a promise that returns the round trip time in milliseconds.
+ * @param url The destination url. E.g. www.google.com
+ * @param port Optional: The port number of the destination. Defaults to 80 
+ * @returns Promise<{responseTime: number }> A promise that returns the round trip time in milliseconds. Returns -1 if an error occurred.
+ * */
+function ping(url: string, port?: number) {
+	var promise = new Promise(resolve => {
+		var result: any;
+		var options = { host: url, port: port || 80, path: '/' };
+		var start = Date.now();
+		var pingRequest = http.request(options, () => { 
+			result = Date.now() - start;
+			resolve(result);
+			pingRequest.abort();
+		});
+
+		pingRequest.on("error", () => {
+			result = -1;
+			resolve(result);
+			pingRequest.abort();
+		});
+		pingRequest.write("");
+		pingRequest.end();
+	});
+	return promise;
+}
+
