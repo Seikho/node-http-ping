e532086b6370dd43e3b356b09eea4bf55e7299b1
Carl Winkler <carl@carlossus.com>
2016-02-18 23:26:12 +0800

    Update tests to expect promise to reject on fail

--- test/index.js
+++ test/index.js
@@ -2,19 +2,22 @@
 var chai = require("chai");
 var ping = require("../src/index");
 var expect = chai.expect;
+// TODO: Create temporary HTTP service for test
 describe("ping tests", function () {
     it("will ping google.com and return a response time", function (done) {
         ping("google.com")
             .then(function (time) {
-            expect(time > 0).to.equal(true);
+            expect(time).to.be.above(0);
             done();
-        }).catch(done);
+        })
+            .catch(done);
     });
     it("will return -1 when a domain cannot be reached", function (done) {
         ping("some-domain-that-better-not-exist.fake")
-            .then(function (time) {
-            expect(time).to.equal(-1);
+            .then(done)
+            .catch(function (error) {
+            expect(error).to.equal(-1);
             done();
-        }).catch(done);
+        });
     });
 });
--- test/index.ts
+++ test/index.ts
@@ -2,20 +2,24 @@ import chai = require("chai");
 import ping = require("../src/index");
 var expect = chai.expect;
 
-describe("ping tests", () => {
-	it("will ping google.com and return a response time", done => {
-		ping("google.com")
-			.then(time => {
-				expect(time > 0).to.equal(true);
-				done();
-			}).catch(done);
-	});
+// TODO: Create temporary HTTP service for test
 
-	it("will return -1 when a domain cannot be reached", done => {
-		ping("some-domain-that-better-not-exist.fake")
-			.then(time => {
-				expect(time).to.equal(-1);
-				done();
-			}).catch(done);
-	});
+describe("ping tests", () => {
+    it("will ping google.com and return a response time", done => {
+        ping("google.com")
+            .then(time => {
+                expect(time).to.be.above(0);
+                done();
+            })
+            .catch(done);
+    });
+
+    it("will return -1 when a domain cannot be reached", done => {
+        ping("some-domain-that-better-not-exist.fake")
+            .then(done)
+            .catch(error => {
+                expect(error).to.equal(-1);
+                done();
+            });
+    });
 });
